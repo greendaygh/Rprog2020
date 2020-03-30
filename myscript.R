6422b7005d576c598775e7a9574b8bc8d891b2d0
haseong <kimhaseong@gmail.com>
2020-03-30 22:11:22 +0900

    update

---
+++ myscript.R
@@ -0,0 +1,5 @@
+numbers <- c(0.452, 1.474, 0.22, 0.545, 1.205, 3.55, 5, 6)
+cat("Numbers are", numbers, "\n")
+numbers_mean <- mean(numbers)
+out <- paste("The average is ", numbers_mean, ".", sep="")
+cat(out)
